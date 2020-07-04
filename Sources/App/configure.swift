d6ad5d10f067ec94b5714b14dd2c39d97153feb1
Gawish <mohd.gawish@gmail.com>
2020-07-04 10:07:58 +0400

    setup database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -12,21 +12,35 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(router, as: Router.self)
 
     // Register middleware
-    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    var middlewares = MiddlewareConfig()
+    middlewares.use(ErrorMiddleware.self)
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    // Configure DB
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else if env == .testing {
+        databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
+                                                  port: 5433,
+                                                  username: "aku-test",
+                                                  database: "aku-test",
+                                                  password: "password")
+    } else {
+        databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
+                                                  port: 5432,
+                                                  username: "aku",
+                                                  database: "aku",
+                                                  password: "password")
+    }
 
     // Register the configured SQLite database to the database config.
+    let database = PostgreSQLDatabase(config: databaseConfig)
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: database, as: .psql)
     services.register(databases)
 
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 }
